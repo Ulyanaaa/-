6876810ee5316072dbc4bb91e2391be3029e0228
Ulyana Yunusova <85567113+Ulyanaaa@users.noreply.github.com>
2023-04-22 19:06:11 +0300

    Add files via upload

---
+++ lab1_5/lab5.cpp
@@ -0,0 +1,37 @@
+﻿/*Задача 1.20
+Составить программу нахождения кинетической  энергии тела,
+массой m,  движущегося со скоростью  V.
+
+С использованием вычисляющей функции, расположенной ниже главной
+функции и использую глобальные переменные
+*/
+
+
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+float global_m = 1;
+float global_v = 1;
+
+void count(float& e);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float e;
+	count(e);
+	cout << endl << "При массе равной " << global_m  << " и при скорости равной " << global_v << " кинетическая энергия равна: " << e << endl;
+	cout << "Введите значение массы:";
+	cin >> global_m;
+	cout << "Введите значение объема:";
+	cin >> global_v;
+	count(e);
+	cout << "Кинетическая энергия равна: " << e;
+}
+void count(float& e)
+{
+	e = (global_m * pow(global_v, 2) / 2);
+}
+
